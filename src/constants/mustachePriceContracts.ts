e404172c7fcbae2072729f411729d029ccc51788
Mustacheswap <83590343+mustacheswap@users.noreply.github.com>
2021-05-19 18:25:42 +0200

    Update mustachePriceContracts.ts

--- src/constants/mustachePriceContracts.ts
+++ src/constants/mustachePriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: 'MustacheContractAddress',
+  cakeAddress: '0x8D4Fe1B9cAd29A81B15984E5249498219d82d4C9',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSDcontract-address 
-  lpAddress: 'Mustache-BUSD LP address'
+  lpAddress: '0x37bc049e55aff4bed7079983f0a172a4d9e3a83f'
 }
 
 export default priceContracts
